329798ce9b0a1f2897b5f0cbc63735fd568822ae
pranamyaghanade884-hub <pranamyaghanade884@gmail.com>
2025-11-08 02:59:58 +0530

    profit loss calculation

---
+++ profit_loss.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    float costPrice, sellingPrice, profit, loss;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &costPrice);
+    printf("Enter Selling Price: ");
+    scanf("%f", &sellingPrice);
+
+    if (sellingPrice > costPrice)
+    {
+        profit = sellingPrice - costPrice;
+        printf("Profit = %.2f\n", profit);
+    }
+    else if (costPrice > sellingPrice)
+    {
+        loss = costPrice - sellingPrice;
+        printf("Loss = %.2f\n", loss);
+    }
+    else
+    {
+        printf("No Profit, No Loss.\n");
+    }
+
+    return 0;
